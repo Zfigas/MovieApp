5a13ba244ef5ff8283924dcc95fb6a9773de922c
zfigas <zfigas@gmail.com>
2018-11-05 10:26:53 +0100

    First commit

---
+++ app/src/main/java/org/example/movieexamapp/MainActivity.java
@@ -0,0 +1,35 @@
+package org.example.movieexamapp;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contactList = new ArrayList<>();
+        lv = findViewById(R.id.list);
+
+        new getMovies().execute();
+    }
+    }
